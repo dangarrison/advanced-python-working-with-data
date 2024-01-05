4df219a187ba126b81d8d434361179e7673883fd
DanGarrison <garrisonda@gmail.com>
2024-01-05 14:07:09 -0800

    ch1 challenge complete and named tuple exercise complete

--- Start/Ch_1/challenge_start.py
+++ Start/Ch_1/challenge_start.py
@@ -13,4 +13,37 @@ import json
 # open the data file and load the JSON
 with open("../../30DayQuakes.json", "r") as datafile:
     data = json.load(datafile)
+
+def getFelt(dataitem):
+    felt = dataitem['properties']['felt']
     
+    if (felt is None):
+        felt = 0
+    return int(felt)
+
+def getSig(dataitem):
+    sig = dataitem['properties']['sig']
+
+    if (sig is None):
+        sig = 0
+    return int(sig)
+    
+quake = str(sum(quake['properties']['felt'] is not None and quake['properties']['felt'] >= 100
+                                                       for quake in data['features']))
+
+
+mostFeltQuake = max(data["features"], key=getFelt)
+
+
+print(f"Total quakes: {len(data['features'])}")
+
+print(f"Total quakes felt by at least 100 people: {quake}")
+
+print(f"Most felt Reports: {mostFeltQuake['properties']['title']}, reports: {mostFeltQuake['properties']['felt']}")
+
+data["features"].sort(key=getSig, reverse=True)
+
+print(data["features"][0]['properties']['title'])
+
+for i in range(0, 10):
+    print(f"Event: {data['features'][i]['properties']['title']}, Significance: {data['features'][i]['properties']['sig']}")
