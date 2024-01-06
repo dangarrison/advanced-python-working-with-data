7d3b50fdc4fcb613c964fb3b2ec641e069370009
DanGarrison <garrisonda@gmail.com>
2024-01-05 18:02:32 -0800

    chapter 2 challenge complete

--- Start/Ch_2/challenge_start.py
+++ Start/Ch_2/challenge_start.py
@@ -2,8 +2,20 @@
 # Programming challenge: use advanced data collections on the earthquake data
 
 import json
-
+from collections import defaultdict
 
 # open the data file and load the JSON
 with open("../../30DayQuakes.json", "r") as datafile:
     data = json.load(datafile)
+   
+
+quake_counter = defaultdict(int)
+
+for quake in data["features"]:
+    quake_counter[quake["properties"]["type"]] += 1
+
+
+for key, value in quake_counter.items():
+    print(f"{key:15}: {value}")
+
+    
